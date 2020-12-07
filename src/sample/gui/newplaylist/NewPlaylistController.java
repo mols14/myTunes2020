f334ab5498c43143b6120db9434e8662542ba036
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2020-12-07 12:10:04 +0100

    controllers done

--- src/sample/gui/newplaylist/NewPlaylistController.java
+++ src/sample/gui/newplaylist/NewPlaylistController.java
@@ -3,6 +3,7 @@ package sample.gui.newplaylist;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,10 +11,23 @@ import java.util.ResourceBundle;
 public class NewPlaylistController implements Initializable {
 
     @FXML
+    private Button cancelPlaylist;
+    @FXML
+    private TextField nameInputField;
+    @FXML
     private Button savePlaylist;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void clickedSavePL(){
+        System.out.println("Save Button is working");
+        //TODO
+    }
+    public void clickedCancelPL(){
+        System.out.println("Cancel Button is working");
+        //TODO
     }
 }
--- src/sample/gui/newsong/newSongController.java
+++ src/sample/gui/newsong/newSongController.java
@@ -3,6 +3,8 @@ package sample.gui.newsong;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,10 +12,42 @@ import java.util.ResourceBundle;
 public class newSongController implements Initializable {
 
     @FXML
+    private SplitMenuButton categoryFolder;
+    @FXML
+    private TextField songFilePathInput;
+    @FXML
+    private TextField timeSongInput;
+    @FXML
+    private TextField artistSongInput;
+    @FXML
+    private TextField titleSongInput;
+    @FXML
+    private Button categoryMoreButton;
+    @FXML
+    private Button fileSongPathChooseButton;
+    @FXML
+    private Button newSongCancel;
+    @FXML
     private Button saveSong;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+    public void clickedFileSOngPathChooseBN(){
+        System.out.println("FileSongPathChooseBN  is working");
+        //TODO
+    }
+    public void clickedMoreCategoryBN(){
+        System.out.println("MoreCategoryBN is working");
+        //TODO
+    }
+    public void clickedNewSongCancel(){
+        System.out.println("newSongCancel Button is working");
+        //TODO
+    }
+    public void clickedSaveSong(){
+        System.out.println("Save Button is working");
+        //TODO
     }
 }
