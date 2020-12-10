cd1bdea52729da5eb9aede80cfadc786e876dbb9
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2020-12-10 10:21:23 +0100

    tilføjet media jar
    begynder at oprette mediaplayer.
    har tilføjet media jars

--- src/sample/bll/util/LogicFacade.java
+++ src/sample/bll/util/LogicFacade.java
@@ -1,4 +1,23 @@
 package sample.bll.util;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+
+
+
 public class LogicFacade {
+
+    private MediaPlayer mediaPlayer;
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void playSong() {
+        String filePath = "C:/Users/Svend Halding/Desktop/New folder/songs/Black.mp3";
+        Media media = new Media(filePath);
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
 }
