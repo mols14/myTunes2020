2e4164e1f98143bfdb2dde4a4e5347d23cf0ac14
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2020-12-14 15:58:08 +0100

    Play button i Logicfacade
    har gjort så play/pause boutton kan både afspille og pause.
    
    og pause ikke bare er genstart men den faktisk starter der hvor der blev stoppet.
    
    ændre sti så det passede ved mig og rasmus også

--- src/sample/bll/util/LogicFacade.java
+++ src/sample/bll/util/LogicFacade.java
@@ -11,15 +11,29 @@ public class LogicFacade {
 
     private MediaPlayer mediaPlayer;
 
+    private boolean isPlaying = false;
+    private boolean isPause = true;
+
     public MediaPlayer getMediaPlayer() {
         return mediaPlayer;
     }
-
     public void playSong() {
-        File file = new File("./myTunes2020/src/music/Home.mp3");
-        String filePath = file.toURI().toString();
-        Media media = new Media(filePath);
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.play();
+        if(!isPlaying && this.mediaPlayer != null){
+            mediaPlayer.play();
+            isPlaying = true;
+        }
+        else if (!isPlaying) {
+            File file = new File("./src/music/Home.mp3");
+            String filePath = file.toURI().toString();
+            Media media = new Media(filePath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.play();
+            isPlaying = true;
+        }
+        else if(isPlaying){
+            mediaPlayer.pause();
+            isPlaying = false;
+        }
+
     }
 }
